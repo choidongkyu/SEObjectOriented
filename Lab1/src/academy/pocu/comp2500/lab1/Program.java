79c1af2f8b86040993d9362cc8c3caf8c90fa135
dkchoi <ehdqkwk@naver.com>
2020-09-07 19:11:45 +0900

    push lab 1

---
+++ Lab1/src/academy/pocu/comp2500/lab1/Program.java
@@ -0,0 +1,11 @@
+package academy.pocu.comp2500.lab1;
+
+import static academy.pocu.comp2500.lab1.Lab1.add;
+
+public class Program {
+
+    public static void main(String[] args) {
+        int a =  add(3, 5);
+        System.out.println(a);
+    }
+}
