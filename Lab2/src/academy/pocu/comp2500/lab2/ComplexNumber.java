b49374998d1520744b2bd7b350fe162cd86192f0
dkchoi <ehdqkwk@naver.com>
2020-09-15 23:38:14 +0900

    lab 2 push

---
+++ Lab2/src/academy/pocu/comp2500/lab2/ComplexNumber.java
@@ -0,0 +1,64 @@
+package academy.pocu.comp2500.lab2;
+
+public class ComplexNumber {
+    final double real;
+    final double imaginary;
+
+
+    public ComplexNumber() {
+        this.real = 0.0;
+        this.imaginary = 0.0;
+    }
+
+    public ComplexNumber(double real) {
+        this.real = real;
+        this.imaginary = 0.0;
+    }
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public boolean isReal() {
+        if (imaginary != 0.0) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isImaginary() {
+        if (real != 0.0 || imaginary == 0.0) {
+            return false;
+        }
+        return true;
+    }
+
+    public ComplexNumber getConjugate() {
+        return new ComplexNumber(real, -imaginary);
+    }
+
+    public ComplexNumber add(ComplexNumber other) {
+        return new ComplexNumber(real + other.real, imaginary + other.imaginary);
+    }
+
+    public ComplexNumber subtract(ComplexNumber other) {
+        return new ComplexNumber(real - other.real, imaginary - other.imaginary);
+    }
+
+    public ComplexNumber multiply(ComplexNumber other) {
+        return new ComplexNumber((real * other.real) - (imaginary * other.imaginary),
+                (real * other.imaginary) + (imaginary * other.real));
+    }
+
+    public ComplexNumber divide(ComplexNumber other) {
+        double i = Math.pow(other.real, 2) + Math.pow(other.imaginary, 2);
+        double _real = ((real * other.real) + (imaginary * other.imaginary)) / i;
+        double _imaginary = ((imaginary * other.real) - (real * other.imaginary)) / i;
+        return new ComplexNumber(_real, _imaginary);
+    }
+
+    public void print() {
+        System.out.println(real + " + " + imaginary + "i");
+    }
+}
