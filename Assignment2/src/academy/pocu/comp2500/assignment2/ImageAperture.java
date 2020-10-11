a88e8ba4ae6cd08af7a6771e6de43b48ad0f44ce
dkchoi <ehdqkwk@naver.com>
2020-10-11 13:59:06 +0900

    fix

--- Assignment2/src/academy/pocu/comp2500/assignment2/ImageAperture.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/ImageAperture.java
@@ -1,47 +1,14 @@
 package academy.pocu.comp2500.assignment2;
 
-public class ImageAperture {
-    private int x;
-    private int y;
-    private Size size;
+public class ImageAperture extends Aperture {
     private String imagePath;
 
     public ImageAperture(int x, int y, String imagePath, Size size) {
-        this.x = x;
-        this.y = y;
+        super(x, y, size);
         this.imagePath = imagePath;
-        this.size = size;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public Size getSize() {
-        return size;
-    }
-
-    public void setSize(Size size) {
-        this.size = size;
     }
 
     public String getImagePath() {
         return imagePath;
-    }
-
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
     }
 }
--- Assignment2/src/academy/pocu/comp2500/assignment2/TextAperture.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/TextAperture.java
@@ -1,47 +1,14 @@
 package academy.pocu.comp2500.assignment2;
 
-public class TextAperture {
-    private int x;
-    private int y;
-    private Size size;
+public class TextAperture extends Aperture {
     private String text;
 
     public TextAperture(int x, int y, String text, Size size) {
-        this.x = x;
-        this.y = y;
+        super(x, y, size);
         this.text = text;
-        this.size = size;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
     }
 
     public String getText() {
         return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Size getSize() {
-        return size;
-    }
-
-    public void setSize(Size size) {
-        this.size = size;
     }
 }
