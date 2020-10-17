c827f5abf0c5da91f7ae29fe1f52b946b6cc5f52
dkchoi <ehdqkwk@naver.com>
2020-10-17 17:46:26 +0900

    fix

--- Lab6/src/academy/pocu/comp2500/lab6/DeathByDesserts.java
+++ Lab6/src/academy/pocu/comp2500/lab6/DeathByDesserts.java
@@ -4,31 +4,13 @@ import java.util.ArrayList;
 
 public class DeathByDesserts extends Combo {
     private static final int PRICE = 20;
-    private ArrayList<Dessert> desserts = new ArrayList<>();
 
     public DeathByDesserts() {
         super(PRICE);
     }
-
-    public ArrayList<Dessert> getDesserts() {
-        return this.desserts;
-    }
+    
 
     public void setDesserts(Dessert dessert1, Dessert dessert2, Dessert dessert3, Dessert dessert4) {
-        this.desserts.clear();
-
-        this.desserts.add(dessert1);
-        this.desserts.add(dessert2);
-        this.desserts.add(dessert3);
-        this.desserts.add(dessert4);
-        this.isAdded = true;
-    }
-
-    /*public ArrayList<Dessert> getDesserts() {
-        return super.desserts;
-    }*/
-
-    /*public void setDesserts(Dessert dessert1, Dessert dessert2, Dessert dessert3, Dessert dessert4) {
         super.desserts.clear();
 
         super.desserts.add(dessert1);
@@ -36,5 +18,5 @@ public class DeathByDesserts extends Combo {
         super.desserts.add(dessert3);
         super.desserts.add(dessert4);
         super.isAdded = true;
-    }*/
+    }
 }
